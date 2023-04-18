f29c4b42d95e5a9576f4d23d68633daac602c060
d3m0nrul3s <122651531+d3m0nrul3s@users.noreply.github.com>
2023-04-18 23:31:11 +0300

    Update eth.sh

--- config/bin/eth.sh
+++ config/bin/eth.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-echo "%{F#2495e7} %{F#ffffff}$(/usr/sbin/ifconfig eth0 | grep "inet " | awk '{print $2}')%{u-}"
+IFACE=$(/usr/sbin/ifconfig | grep eth0 | awk '{print $1}' | tr -d ':')
+
+if [ "$IFACE" = "eth0" ]; then
+        echo "%{F#1bbf3e} %{F#ffffff}$(/usr/sbin/ifconfig eth0 | grep "inet " | awk '{print $2}')%{u-}"
+else
+        echo "%{F#2495e7}%{u-} %{F#ffffff}Disconnected"
+fi
